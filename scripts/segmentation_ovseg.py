7188b9793c6c5f4643842301c3a617a87f9a827d
Hermann <blumh@ethz.ch>
2023-04-06 12:53:26 +0200

    ovseg prompt ablation

--- scripts/segmentation_ovseg.py
+++ scripts/segmentation_ovseg.py
@@ -12,7 +12,7 @@ from detectron2.projects.deeplab import add_deeplab_config
 from detectron2.data.detection_utils import read_image
 from detectron2.utils.logger import setup_logger
 
-from segmentation_tools.label_data import get_ade150, get_wordnet
+from segmentation_tools.label_data import get_ade150, get_wordnet, get_replica
 import argparse
 import logging
 from pathlib import Path
@@ -20,6 +20,7 @@ from tqdm import tqdm
 import cv2
 import torch
 import numpy as np
+import shutil
 
 logging.basicConfig(level="INFO")
 log = logging.getLogger('OV-Seg Segmentation')
@@ -27,15 +28,17 @@ log = logging.getLogger('OV-Seg Segmentation')
 
 class WordnetPromptTemplate:
 
-    def __init__(self, template):
+    def __init__(self, template, add_synonyms=True):
         self.template = template
+        self.add_synonyms = add_synonyms
 
     def format(self, noun):
         synset = wn.synset(noun)
         prompt = self.template.format(noun=synset.name().split('.')[0],
-                                    definition=synset.definition())
-        if len(synset.lemma_names()) > 1:
-            prompt += " It can also be called {}".format(", ".join(synset.lemma_names()[1:]))
+                                      definition=synset.definition())
+        if self.add_synonyms and len(synset.lemma_names()) > 1:
+            prompt += " It can also be called {}".format(", ".join(
+                synset.lemma_names()[1:]))
         return prompt
 
     def __str__(self):
@@ -78,10 +81,16 @@ def process_image(model, img_path, class_names, threshold=0.7):
     return pred
 
 
-def ovseg_inference(scene_dir, keys, classes='wordnet', img_template='color/{k}.jpg'):
-    log.info(f'using {flags.classes} classes')
+def ovseg_inference(scene_dir,
+                    keys,
+                    classes='wordnet',
+                    img_template='color/{k}.jpg'):
+    log.info(f'[ov-seg] using {classes} classes')
+    templates = None
     if classes == 'ade150':
         class_names = [x['name'] for x in get_ade150()]
+    elif classes == 'replica':
+        class_names = [x['name'] for x in get_replica()]
     elif classes == 'wordnet':
         sizeless_templates = [
             "a photo of a {size}{noun}, which is {definition}.",
@@ -102,26 +111,98 @@ def ovseg_inference(scene_dir, keys, classes='wordnet', img_template='color/{k}.
             for s in ["", "small ", "medium ", "large "]:
                 templates.append(
                     WordnetPromptTemplate(
-                        t.format(size=s, noun="{noun}",
-                                definition="{definition}")))
-        class_names = [x['name'] for x in get_wordnet()]
+                        t.format(size=s,
+                                 noun="{noun}",
+                                 definition="{definition}")))
+        # the first class is the background class
+        class_names = [x['name'] for x in get_wordnet()[1:]]
+    elif classes == 'wn_nosyn':
+        sizeless_templates = [
+            "a photo of a {size}{noun}, which is {definition}.",
+            "a photo of a {size}{noun}, which can be defined as {definition}.",
+            "a photo of a {size}{noun}, as in {definition}.",
+            "This is a photo of a {size}{noun}, which is {definition}",
+            "This is a photo of a {size}{noun}, which can be defined as {definition}",
+            "This is a photo of a {size}{noun}, as in {definition}",
+            "There is a {size}{noun} in the scene",
+            "There is a {size}{definition} in the scene",
+            "There is the {size}{noun} in the scene",
+            "There is the {size}{definition} in the scene",
+            "a photo of a {size}{noun} in the scene",
+            "a photo of a {size}{definition} in the scene",
+        ]
+        templates = []
+        for t in sizeless_templates:
+            for s in ["", "small ", "medium ", "large "]:
+                templates.append(
+                    WordnetPromptTemplate(
+                        t.format(size=s,
+                                 noun="{noun}",
+                                 definition="{definition}"), add_synonyms=False))
+        # the first class is the background class
+        class_names = [x['name'] for x in get_wordnet()[1:]]
+    elif classes == 'wn_nodef':
+        sizeless_templates = [
+            "a photo of a {size}{noun}",
+            "a photo of a {size}{noun}",
+            "a photo of a {size}{noun}",
+            "This is a photo of a {size}{noun}.",
+            "This is a photo of a {size}{noun}.",
+            "This is a photo of a {size}{noun}.",
+            "There is a {size}{noun} in the scene",
+            "There is the {size}{noun} in the scene",
+            "a photo of a {size}{noun} in the scene",
+        ]
+        templates = []
+        for t in sizeless_templates:
+            for s in ["", "small ", "medium ", "large "]:
+                templates.append(
+                    WordnetPromptTemplate(
+                        t.format(size=s,
+                                 noun="{noun}")))
+        # the first class is the background class
+        class_names = [x['name'] for x in get_wordnet()[1:]]
+    elif classes == 'wn_nosyn_nodef':
+        sizeless_templates = [
+            "a photo of a {size}{noun}",
+            "a photo of a {size}{noun}",
+            "a photo of a {size}{noun}",
+            "This is a photo of a {size}{noun}.",
+            "This is a photo of a {size}{noun}.",
+            "This is a photo of a {size}{noun}.",
+            "There is a {size}{noun} in the scene",
+            "There is the {size}{noun} in the scene",
+            "a photo of a {size}{noun} in the scene",
+        ]
+        templates = []
+        for t in sizeless_templates:
+            for s in ["", "small ", "medium ", "large "]:
+                templates.append(
+                    WordnetPromptTemplate(
+                        t.format(size=s,
+                                 noun="{noun}"), add_synonyms=False))
+        # the first class is the background class
+        class_names = [x['name'] for x in get_wordnet()[1:]]
     else:
-        raise ValueError(f'Unknown class set {flags.classes}')
+        raise ValueError(f'Unknown class set {classes}')
 
-    log.info('loading model')
-    if classes == 'wordnet':
+    log.info('[ov-seg] loading model')
+    if templates is not None:
         model = load_ovseg(custom_templates=templates)
     else:
         model = load_ovseg()
 
+    log.info('[ov-seg] inference')
     scene_dir = Path(scene_dir)
     assert scene_dir.exists() and scene_dir.is_dir()
-    (scene_dir / f'pred_ovseg_{flags.classes}').mkdir(exist_ok=True)
+    shutil.rmtree(scene_dir / f'pred_ovseg_{classes}', ignore_errors=True)
+    (scene_dir / f'pred_ovseg_{classes}').mkdir(exist_ok=False)
     for k in tqdm(keys):
         img = str(scene_dir / img_template.format(k=k))
         result = process_image(model, img, class_names)
-        cv2.imwrite(str(scene_dir / f'pred_ovseg_{flags.classes}' / f'{k}.png'),
-                    result + 1)  # png cannot save -1
+        cv2.imwrite(str(scene_dir / f'pred_ovseg_{classes}' / f'{k}.png'),
+                    result + 1)  # if 0 is background
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -141,4 +222,7 @@ if __name__ == '__main__':
             int(x.name.split('.')[0]) for x in (scene_dir / 'color').iterdir())
         img_template = 'color/{k}.png'
     log.info('running inference')
-    ovseg_inference(scene_dir, keys, classes=flags.classes, img_template=img_template)
+    ovseg_inference(scene_dir,
+                    keys,
+                    classes=flags.classes,
+                    img_template=img_template)
