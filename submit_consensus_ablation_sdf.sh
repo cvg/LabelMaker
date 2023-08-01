f2b03e46e8425c7bff68c9bf58d8ba2cb88b1d65
Silvan Weder <silvan.weder@inf.ethz.ch>
2023-08-02 00:12:19 +0200

    reorg, consensus ablation sdf scripts

---
+++ submit_consensus_ablation_sdf.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# no_cmx
+source activate venv/bin/activate
+sbatch --parsable --gpus=v100:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_cmx.bash scene0000_00"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_cmx.bash scene0164_02"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_cmx.bash scene0458_00"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_cmx.bash scene0474_01"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_cmx.bash scene0518_00"
+
+# no_intern
+source activate venv/bin/activate
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_intern.bash scene0000_00"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_intern.bash scene0164_02"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_intern.bash scene0458_00"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_intern.bash scene0474_01"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_intern.bash scene0518_00"
+
+# no_mask3d
+source activate venv/bin/activate
+sbatch --parsable --gpus=v100:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_mask3d.bash scene0000_00"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_mask3d.bash scene0164_02"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_mask3d.bash scene0458_00"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_mask3d.bash scene0474_01"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_mask3d.bash scene0518_00"
+
+# no_ovseg
+source activate venv/bin/activate
+sbatch --parsable --gpus=v100:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_ovseg.bash scene0000_00"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_ovseg.bash scene0164_02"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_ovseg.bash scene0458_00"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_ovseg.bash scene0474_01"
+sbatch --parsable --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_ovseg.bash scene0518_00"
