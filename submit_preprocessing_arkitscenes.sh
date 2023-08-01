a91c75ef3c0d5039a4691a14214983599fde1651
Silvan Weder <silvan.weder@inf.ethz.ch>
2023-08-01 09:40:36 +0200

    small fix

--- submit_preprocessing_arkitscenes.sh
+++ submit_preprocessing_arkitscenes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bash scripts/eulerpreprocessarkitscenesbash 42445991  
-bash scripts/eulerpreprocessarkitscenesbash 42446517  
-bash scripts/eulerpreprocessarkitscenesbash 42446527  
-bash scripts/eulerpreprocessarkitscenesbash 42897521	
-bash scripts/eulerpreprocessarkitscenesbash 42897688
+bash scripts/eulerpreprocessarkitscenes.bash 42445991  
+bash scripts/eulerpreprocessarkitscenes.bash 42446517  
+bash scripts/eulerpreprocessarkitscenes.bash 42446527  
+bash scripts/eulerpreprocessarkitscenes.bash 42897521	
+bash scripts/eulerpreprocessarkitscenes.bash 42897688
