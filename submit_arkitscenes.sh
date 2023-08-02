cfca35741738a5ac4c577b8e94e4a30d25c7da83
Silvan Weder <silvan.weder@inf.ethz.ch>
2023-08-02 22:52:31 +0200

    arkitscenes submission

---
+++ submit_arkitscenes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sbatch < scripts/arkitscenes/42445991sdf_class_weights.sh
+sbatch < scripts/arkitscenes/42446517sdf_class_weights.sh
+sbatch < scripts/arkitscenes/42446527sdf_class_weights.sh
+sbatch < scripts/arkitscenes/42897521sdf_class_weights.sh
+sbatch < scripts/arkitscenes/42897688sdf_class_weights.sh
