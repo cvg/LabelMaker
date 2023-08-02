bd2c8210716484998f4aea97acd12b2c075e5c56
Silvan Weder <silvan.weder@inf.ethz.ch>
2023-08-02 09:54:14 +0200

    env fix

--- submit_consensus_ablation_sdf.sh
+++ submit_consensus_ablation_sdf.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+source euler_env
+sleep 10
+
 commonargsnogpu="--parsable -n 1 --time=48:00:00 --mem-per-cpu=8000 --tmp=16000 --mail-type=END,FAIL --mail-user=weders@ethz.ch"
 
 # no_cmx
 source venv/bin/activate
+sleep 10
+
 sbatch $commonargsnogpu --gpus=v100:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_cmx.bash scene0000_00"
+
 source venv3090/bin/activate
+sleep 10
+
 sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_cmx.bash scene0164_02"
 sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_cmx.bash scene0458_00"
 sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_cmx.bash scene0474_01"
@@ -13,8 +21,14 @@ sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablatio
 
 # no_intern
 source venv/bin/activate
+sleep 10
+
 sbatch $commonargsnogpu --gpus=v100:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_intern.bash scene0000_00"
+
+
 source venv3090/bin/activate
+sleep 10
+
 sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_intern.bash scene0164_02"
 sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_intern.bash scene0458_00"
 sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_intern.bash scene0474_01"
@@ -22,8 +36,13 @@ sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablatio
 
 # no_mask3d
 source venv/bin/activate
+sleep 10
+
 sbatch $commonargsnogpu --gpus=v100:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_mask3d.bash scene0000_00"
+
 source venv3090/bin/activate
+sleep 10
+
 sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_mask3d.bash scene0164_02"
 sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_mask3d.bash scene0458_00"
 sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_mask3d.bash scene0474_01"
@@ -31,8 +50,13 @@ sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablatio
 
 # no_ovseg
 source activate venv/bin/activate
+sleep 10
+
 sbatch $commonargsnogpu --gpus=v100:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_ovseg.bash scene0000_00"
+
 source venv3090/bin/activate
+sleep 10
+
 sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_ovseg.bash scene0164_02"
 sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_ovseg.bash scene0458_00"
 sbatch $commonargsnogpu --gpus=rtx_3090:1 --wrap="bash scripts/consensus_ablation/sdf_class_weights_no_ovseg.bash scene0474_01"
