096cba03ba5bd8582799756231723f1661909119
Silvan Weder <silvan.weder@inf.ethz.ch>
2023-08-03 14:33:57 +0200

    changed time

--- scripts/arkitscenes/42897521sdf_class_weights.bash
+++ scripts/arkitscenes/42897521sdf_class_weights.bash
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --gpus=rtx_3090:1
 #SBATCH --gres=gpumem:11264m
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=8000
 #SBATCH --tmp=16000
 #SBATCH --mail-type=END,FAIL
