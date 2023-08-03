0d7134a07cf67867bbedc349c44d5e80329aa092
Silvan Weder <silvan.weder@inf.ethz.ch>
2023-08-03 20:13:07 +0200

    new experiments

--- submit_replica.sh
+++ submit_replica.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sbatch < scripts/replica/replicasdf_office_0.bash
-sbatch < scripts/replica/replicasdf_office_1.bash
+# sbatch < scripts/replica/replicasdf_office_0.bash
+# sbatch < scripts/replica/replicasdf_office_1.bash
 sbatch < scripts/replica/replicasdf_office_2.bash
 sbatch < scripts/replica/replicasdf_office_3.bash
 sbatch < scripts/replica/replicasdf_office_4.bash
