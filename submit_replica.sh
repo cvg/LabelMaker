2a9df5cc131129246bc1ed0799e20d673f53c11f
Silvan Weder <silvan.weder@inf.ethz.ch>
2023-08-03 20:01:19 +0200

    replica script

---
+++ submit_replica.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sbatch < scripts/replica/replicasdf_office_0.bash
+sbatch < scripts/replica/replicasdf_office_1.bash
+sbatch < scripts/replica/replicasdf_office_2.bash
+sbatch < scripts/replica/replicasdf_office_3.bash
+sbatch < scripts/replica/replicasdf_office_4.bash
+sbatch < scripts/replica/replicasdf_room_0.bash
+sbatch < scripts/replica/replicasdf_room_1.bash
+sbatch < scripts/replica/replicasdf_room_2.bash
+
+
+
