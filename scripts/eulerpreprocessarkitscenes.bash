2f04c6877fbc4236324d5e31a017d5a3d485cc11
Silvan Weder <silvan.weder@inf.ethz.ch>
2023-08-24 09:56:30 +0200

    fixed arkitsceens preprocessing

--- scripts/eulerpreprocessarkitscenes.bash
+++ scripts/eulerpreprocessarkitscenes.bash
@@ -11,17 +11,17 @@ normals=$(sbatch $commonargs --wrap="bash -c './scripts/copy_arkitscenes.bash $s
 depth=$(sbatch $commonargs --time=04:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && python scripts/depthregression_omnidata.py --scene \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/omnidata_depth /cluster/project/cvg/blumh/arkitscenes/$scene/ && python scripts/depth2hha.py \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/hha /cluster/project/cvg/blumh/arkitscenes/$scene/'")
 
 # uncomment below
-# seg1=$(sbatch $commonargs --time=04:00:00  --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && python scripts/segmentation_ovseg.py --classes wn_nodef \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_ovseg_wn_nodef /cluster/project/cvg/blumh/arkitscenes/$scene/'")
-# seg2=$(sbatch $commonargs --time=04:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && python scripts/segmentation_ovseg.py --classes wn_nodef --flip \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_ovseg_wn_nodef_flip /cluster/project/cvg/blumh/arkitscenes/$scene/'")
+seg1=$(sbatch $commonargs --time=04:00:00  --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && python scripts/segmentation_ovseg.py --classes wn_nodef \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_ovseg_wn_nodef /cluster/project/cvg/blumh/arkitscenes/$scene/'")
+seg2=$(sbatch $commonargs --time=04:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && python scripts/segmentation_ovseg.py --classes wn_nodef --flip \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_ovseg_wn_nodef_flip /cluster/project/cvg/blumh/arkitscenes/$scene/'")
 
-# seg3=$(sbatch $commonargs --time=04:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && python scripts/segmentation_internimage.py \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_internimage /cluster/project/cvg/blumh/arkitscenes/$scene/'")
-# seg4=$(sbatch $commonargs --time=04:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && python scripts/segmentation_internimage.py --flip \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_internimage_flip /cluster/project/cvg/blumh/arkitscenes/$scene/'")
+seg3=$(sbatch $commonargs --time=04:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && python scripts/segmentation_internimage.py \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_internimage /cluster/project/cvg/blumh/arkitscenes/$scene/'")
+seg4=$(sbatch $commonargs --time=04:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && python scripts/segmentation_internimage.py --flip \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_internimage_flip /cluster/project/cvg/blumh/arkitscenes/$scene/'")
 
-# sam=$(sbatch $commonargs --time=22:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && python scripts/segmentation_sam.py \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_sam /cluster/project/cvg/blumh/arkitscenes/$scene/'")
+sam=$(sbatch $commonargs --time=22:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && python scripts/segmentation_sam.py \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_sam /cluster/project/cvg/blumh/arkitscenes/$scene/'")
 
-# #  -d afterany:$depth 
-# seg5=$(sbatch $commonargs -d afterany:$depth --time=04:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && cp -r $scenefolder/hha \$TMPDIR/$scene/ && python scripts/segmentation_cmx.py \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_cmx /cluster/project/cvg/blumh/arkitscenes/$scene/'")
-# seg6=$(sbatch $commonargs -d afterany:$depth --time=04:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && cp -r $scenefolder/hha \$TMPDIR/$scene/ && python scripts/segmentation_cmx.py --flip \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_cmx_flip $scenefolder/'")
+#  -d afterany:$depth 
+seg5=$(sbatch $commonargs -d afterany:$depth --time=04:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && cp -r $scenefolder/hha \$TMPDIR/$scene/ && python scripts/segmentation_cmx.py \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_cmx /cluster/project/cvg/blumh/arkitscenes/$scene/'")
+seg6=$(sbatch $commonargs -d afterany:$depth --time=04:00:00 --wrap="bash -c './scripts/copy_arkitscenes.bash $scene && cp -r $scenefolder/hha \$TMPDIR/$scene/ && python scripts/segmentation_cmx.py --flip \$TMPDIR/$scene && cp -r \$TMPDIR/$scene/pred_cmx_flip $scenefolder/'")
 
 # sbatch $commonargsnogpu --time=04:00:00 -d afterany:$seg1,afterany:$seg2,afterany:$seg3,afterany:$seg4,afterany:$seg5,afterany:$seg6 --wrap="bash scripts/arkitscenesconsensus.bash $scene"
 
