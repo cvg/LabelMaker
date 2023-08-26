ab4a28768fc6fb2695ac37deb17413c223c1762e
Silvan Weder <silvan.weder@inf.ethz.ch>
2023-08-26 07:51:21 +0200

    fixed path

--- scripts/copy_arkitscenes.bash
+++ scripts/copy_arkitscenes.bash
@@ -5,7 +5,7 @@ mkdir $TMPDIR/$1
 for SUBDIR in color depth intrinsic pose 
 do
     echo $SUBDIR
-    cp -r /cluster/project/cvg/blumh/arkitscenes/$1/$SUBDIR $TMPDIR/$1/
+    cp -r /cluster/project/cvg/blumh/arkitscenes_new/$1/$SUBDIR $TMPDIR/$1/
     ls $TMPDIR/$1/$SUBDIR | head
 done
 
