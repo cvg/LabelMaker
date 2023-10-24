2052d065f9516c033c5dc17b701febfb5388e291
Guangda Ji <quanta.guangda.ji@gmail.com>
2023-10-24 20:58:06 +0200

    finish pytest

---
+++ testing/test_models/test_internimage.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from os.path import abspath, dirname, exists, join
+from pathlib import Path
+
+import cv2
+import numpy as np
+
+sys.path.append(abspath(join(dirname(__file__), '../..')))
+
+
+def test_internimage():
+  from models.internimage import run as run_internimage
+
+  scene_dir = Path(abspath(join(dirname(__file__), '../test_scan')))
+  output_folder = 'intermediate/ade20k_internimage_1'
+  name = '000000.png'
+
+  run_internimage(
+      scene_dir=scene_dir,
+      output_folder=output_folder,
+      device='cuda:0',
+  )
+
+  img_arr = cv2.imread(str(scene_dir / output_folder / name),
+                       cv2.IMREAD_UNCHANGED)
+  assert np.unique(img_arr).shape[0] > 1
